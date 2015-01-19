f980b5d6baac018e49a163cba5be692f9cdd3fac
Mehmet KOÇAK <mkocak@kodofisi.org>
2015-01-19 15:58:09 +0200

    SMTP PHP Contact Form
    This Contact Form using a SMTP. We are using the SMTP is not the PHP
    mail() function because the mail function can be attached to the mails
    sent by the ip filter . Can fall to junk box.
    Used by many open-source projects: Wordpress, Drupal, 1CRM, SugarCRM,
    Yii, Joomla! and many more Integrated SMTP support - send without a
    local mail server SMTP authentication with LOGIN, PLAIN, NTLM and
    CRAM-MD5 mechanisms over SSL and TLS transports

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<?php
+/*~ index.php
+.---------------------------------------------------------------------------.
+|  Software: SMTP PHP Contact Form                                          |
+|   Version: 1.0                                                            |
+|   Contact: kodofisi.org											        |
+|      Info: http://kodofisi.org			                                |
+|   Support: http://kodofisi.org			         	                    |
+| ------------------------------------------------------------------------- |
+|     Admin: Mehmet Koçak (project admininistrator)                         |
+| Copyright (c) 2014-2015, Mehmet Koçak. All Rights Reserved.               |
+| ------------------------------------------------------------------------- |
+*/
+?>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>PHP Contact Form - kocakmhmt</title>
+		<meta name="description" content="SMTP PHP Contact Form / kodofisi.org">
+		<meta name="author" content="Mehmet Koçak">
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="style.css" />
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+			<script type="text/javascript"> 
+			function gostergizle()
+			{
+			$("div#container").show(100); 
+			}
+			function sendmessage(){ 
+			var veriler = $('#contact_form').serialize(); 
+			$.ajax({ 
+			type: "POST", 
+			url: "send.php", 
+			data: veriler, 
+			success:function(cevap){ 
+			$("#send").html(""+cevap); 
+			$("div#container").hide(100); 
+			} 
+			})}; 
+			</script>
+			
+</head>
+
+<body>
+		<center><img src="logo.png" alt="kocakmhmt logo"></center>
+		
+		<div id="container">
+			<h2>Contact Form</h2>
+	<form class="form" id="contact_form" name="contact_form" method="post" action="javascript:void(0);">
+		<p class="name">
+			<input type="text" name="name" id="name" placeholder="Your Name*"  required/>
+			<label for="name">Name Surname<font color="red">*</font></label>
+		</p>
+		<p class="email">
+			<input type="email" name="email" id="email" placeholder="mail@example.com*" required />
+			<label for="email">Email<font color="red">*</font></label>
+		</p>
+		<p class="phone">
+			<input type="tel" name="phone" id="phone" placeholder="+00123456789*"  required/>
+			<label for="email">Phone<font color="red">*</font></label>
+		</p>
+		<p class="web">
+			<input type="url" name="web" id="web" placeholder="www.example.com" />
+			<label for="web">Website</label>
+		</p>
+		<p class="sec">
+		<select name="subject">
+		  <option value="General">General</option>
+		  <option value="product">Product Support</option>
+		  <option value="new">New Business</option>
+		  <option value="other">Other</option>
+		</select>
+		<label>Contact Reason</label>
+		</p>
+		<p class="text">
+			<textarea name="text" id="text"placeholder="Your Message*"  required/></textarea>
+		</p>
+		
+		<p class="submit">
+			<input type="submit" onclick="sendmessage();" value="Send" />
+			<input type="reset" value="Reset" />
+		</p>
+	</form>
+	
+	</div>
+	<div id="send"></div>
+	<center>Mehmet Koçak © 2015 | <a href="license.txt">GNU/GPL</a></center>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47181399-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+	</body>
+</html>
---
+++ send.php
@@ -0,0 +1,52 @@
+<?php
+if($_POST)
+{
+$text=$_POST['text'];
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$web=$_POST['web'];
+$domain=$_SERVER['HTTP_HOST'];
+$ipadress=$_SERVER['REMOTE_ADDR'];
+$date = date("d.m.Y"); 
+$time = date("H:i:s"); 
+
+if(empty($name)){
+		echo "<center>Please enter your <b>Name</b> <p><input type='submit' onclick='gostergizle();' value='Back' /></p></center>";
+}
+elseif(empty($email)){
+		echo "<center>Please enter your <b>e-mail</b> <p><input type='submit' onclick='gostergizle();' value='Back' /></p></center>";
+}
+elseif(empty($phone)){
+		echo "<center>Please enter your <b>phone</b> <p><input type='submit' onclick='gostergizle();' value='Back' /></p></center>";
+}
+elseif(empty($text))
+{
+	echo "<center>Please enter your <b>message</b> <p><input type='submit' onclick='gostergizle();' value='Back' /></p></center>";
+}
+	else{
+		
+require("class.phpmailer.php");
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Host     = "mail.kodofisi.org"; // SMTP servers
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "mkocak@kodofisi.org";  // SMTP username
+$mail->Password = ""; // SMTP password
+$mail->From     = "mkocak@kodofisi.org"; // it must be a match with SMTP username
+$mail->Fromname = "Mehmet Koçak"; // from name
+$mail->AddAddress("mkocak@kodofisi.org","Mehmet Koçak"); // SMTP username , Name Surname
+$mail->Subject  =  $_POST['subject'];
+$content = "<h2>You have a message by $domain</h2>  <p><b>Name:</b>$name</p> <p><b>E-Mail:</b>$email</p> <p><b>Phone:</b>$phone</p> <p><b>Subject:</b>$subject</p> <p><b>Message:</b>$text</p> <p><h5>Date: $date . $time </h5></p> <p><h5>IP Adress of User: $ipadress </p>";
+$mail->MsgHTML($content);
+if(!$mail->Send())
+{
+   echo "<center>Error! Its wrong!</center>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+echo "<center>Thank you! Your message has reached us! <p><input type='submit' onclick='gostergizle();' value='Back' /></p></center>";
+}
+}
+?>
